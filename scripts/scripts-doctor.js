8acafe82bfe868a92b9547663c04a9fcf2cf9af6
VERA <viraxyz@ukr.net>
2022-06-13 03:17:13 +0300

    Add onclick form

--- scripts/scripts-doctor.js
+++ scripts/scripts-doctor.js
@@ -6,3 +6,24 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const call = document.getElementById('call');
+
+  if (call.style.display === 'none'){
+    call.style.display = 'block';
+  } else {
+    call.style.display = 'none';
+  }
+});
+
+let userName = '';
+let userPhone = '';
+
+const recall = document.getElementById('recall');
+recall.addEventListener('click', () => {
+  alert(`${userName}, we'd call you in a few minutes!`)
+
+});
