5afced19cf02d66fb486d8565605ddc8337f4f6e
VERA <viraxyz@ukr.net>
2022-07-14 00:28:52 +0300

    add burger menu create by js

--- scripts/scripts-doctor.js
+++ scripts/scripts-doctor.js
@@ -1,16 +1,77 @@
-function myFunction() {
-    var x = document.getElementById("burger");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+function swapMenuButton(){
+  let image = document.getElementById('burger-but');
+    if (image.src.match('images/lines-w.png')){
+      image.src = 'images/close-button.png';
     } else {
-      x.style.display = "block";
+      image.src = 'images/lines-w.png';
     }
   }
 
+function removeClassInLi(){
+  let list = document.getElementsByClassName('menu-child');
+  for (let i = list.length - 1; i >= 0; i--) {
+    let inactiveClass = list[i];
+    inactiveClass.classList.remove('menu-child');
+  } 
+}
+
+function createBurgerMenu(){ 
+  let burgerBody = document.createElement('div'); 
+
+  let burgerMenu = document.getElementById('menu-list');
+  burgerMenu.classList.remove('menu');  
+
+  let liActiv = document.getElementById('activ')
+  liActiv.removeAttribute('id');
+
+  removeClassInLi();
+  
+  let contactButton = document.createElement('div');
+  contactButton.innerHTML = 'Agendar consulta';
+  contactButton.classList.add('button');
+
+  let socialButtons = document.getElementById('social');
+  socialButtons.classList.replace('social', 'social1');  
+
+  burgerBody.appendChild(burgerMenu); 
+  burgerBody.appendChild(contactButton);
+  burgerBody.appendChild(socialButtons);
+
+  let placeForBurger = document.getElementById('burger');
+  placeForBurger.appendChild(burgerBody); 
+}
+
+function showBurgerMenu(){
+  let show = document.getElementById('burger');
+  let displayForshow = getComputedStyle(show).getPropertyValue('display');
+  if (displayForshow === 'none'){
+    show.style.display = 'block';
+  } else {
+    show.style.display = 'none';
+  }
+}
+
+function removeBurgerMenu(){  
+  document.getElementById('burger-but').addEventListener("click", function(){
+    swapMenuButton();
+    showBurgerMenu();
+    if (document.getElementById('burger').hasChildNodes() === false){
+      createBurgerMenu();
+    } else {
+      document.getElementById('burger').removeChild(document.getElementById('burger').firstChild);
+    }
+  });
+}
+removeBurgerMenu();
+
+
+
+  
+
 function showModal(){  
   let call = document.getElementById('call');
-  let displyForCall = getComputedStyle(call).getPropertyValue("display");
-  if (displyForCall === 'none'){
+  let displayForCall = getComputedStyle(call).getPropertyValue('display');
+  if (displayForCall === 'none'){
     call.style.display = 'block';
   } else {
     call.style.display = 'none';
